a9173111a7b7ac26085af770d496728d306f2d7a
Alexisorzabal <106978463+Alexisorzabal@users.noreply.github.com>
2023-08-18 17:58:52 -0300

    react amarena

---
+++ src/components/layout/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import '../../styles/footer.css'
+
+export const Footer = (props) => {
+  return (
+    <footer>
+      <h3>Creado Por Alexis Orzabal 2023</h3>
+      </footer>
+  )
+}
+export default Footer;
---
+++ src/components/layout/Header.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import '../../styles/header.css'
+
+export const Header = (props) => {
+  return (
+    <header>
+    <div>
+      <div class="logo">
+            <img src="img/logo.png" width="200" alt="Amarena"/>
+        </div>
+    </div>
+    </header>
+  )
+}
+export default Header;
